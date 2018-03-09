6c4f84676e1a35b841a83a2ab50b612c9bbda682
vincenthend <vincenthendrha@gmail.com>
2018-03-09 11:34:30 +0700

    Fixed camera controls

--- js/viewer.js
+++ js/viewer.js
@@ -9,30 +9,27 @@ function initViewport(){
 
     renderer.setSize(viewport.clientWidth, viewport.clientHeight);
     viewport.appendChild( renderer.domElement );
-    controls = new THREE.OrbitControls(camera, renderer.domElement );
-    controls.addEventListener( 'change', render );
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
 }
 
 function drawObject(){    
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+    var material = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
+	var cube = new THREE.Mesh( geometry, material );
+	var controls = new THREE.OrbitControls( camera );
     scene.add( cube );
     camera.position.z = 5;
+	controls.update();
+
+	var animate = function () {
+		requestAnimationFrame( animate );
+
+		controls.update();
+
+		renderer.render(scene, camera);
+	};
+	animate();
 }
 
-var animate = function () {
-    requestAnimationFrame( animate );
-
-    //cube.rotation.x += 0.1;
-    //cube.rotation.y += 0.1;
-    controls.update();
-
-    renderer.render(scene, camera);
-};
 
 initViewport();
-drawObject();
-animate();
+var obj = drawObject();
